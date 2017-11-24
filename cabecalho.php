980a7d69167e0cb370b6e092844cdb461c119621
ANDREIR PIRES DE OLIVEIRA <andreir.poliveira@senacsp.edu.br>
2017-11-24 19:26:04 -0200

    index.php

---
+++ cabecalho.php
@@ -0,0 +1,12 @@
+
+		<div class="container">
+		<a href="index.php" class="col-md-4 col-sm-4 col-xs-4"><img src="img/bikcraft.png" alt="Logo Marca Bikcraft"></a>
+		<nav class="col-md-8 col-sm-8 col-xs-8 header_menu">
+			<ul>
+				<li><a href="sobre.php">Sobre</a></li>
+    			<li><a href="produtos.html">Produtos</a></li>
+    			<li><a href="portfolio.html">Portfólio</a></li>
+    			<li><a href="contato.html">Contato</a></li>
+    		</ul>
+		</nav>
+		</div>
